53e9005f3243ca1140e06d931ebdb934dc76f44e
taegeon <g7193g@naver.com>
2025-02-17 13:56:23 +0900

    [create] create project

---
+++ src/main/java/com/example/graduation_work_BE/GraduationWorkBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.graduation_work_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationWorkBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduationWorkBeApplication.class, args);
+	}
+
+}
