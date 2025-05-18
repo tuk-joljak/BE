b1a1243fab191c122d749559b321a0b13c7fcfd2
taegeon <g7193g@naver.com>
2025-05-18 22:36:13 +0900

    fix : fix test CD

--- src/main/java/com/example/graduation_work_BE/GraduationWorkBeApplication.java
+++ src/main/java/com/example/graduation_work_BE/GraduationWorkBeApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class GraduationWorkBeApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hi ci/cd test 이제 좀 됐으면 좋겠다 흐에 히히 몇번 째 수정이냐 히히 또 바꿔?");
+		System.out.println("hi ci/cd test 이제 좀 됐으면 좋겠다 흐에 히히 몇번 째 수정이냐 히히 또 바꿔? 하 진짜 그만 바꾸고 싶다");
 		SpringApplication.run(GraduationWorkBeApplication.class, args);
 	}
 
